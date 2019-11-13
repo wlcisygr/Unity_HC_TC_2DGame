4e028062a0bb8b87f6254c1666fb1205d04966d9
KID <50666207+KID421@users.noreply.github.com>
2019-11-13 21:30:26 +0800

    完成地板移動

--- UnityProject/Assets/Scripts/Ground.cs
+++ UnityProject/Assets/Scripts/Ground.cs
@@ -4,4 +4,11 @@ public class Ground : MonoBehaviour
 {
     [Header("速度"), Range(0.1f, 100f)]
     public float speed = 1.5f;
+
+    public Transform ground;
+
+    private void Update()
+    {
+        ground.Translate(-speed, 0, 0);
+    }
 }
