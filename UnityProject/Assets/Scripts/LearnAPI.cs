a8d0c79c56e41ebdb51e53871452009172ace6a9
KID <50666207+KID421@users.noreply.github.com>
2019-11-13 20:49:42 +0800

    靜態成員與方法使用方式

---
+++ UnityProject/Assets/Scripts/LearnAPI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("一般!");
+        Debug.LogWarning("警告!");
+        Debug.LogError("錯誤!");
+
+        Debug.Log(Mathf.PI);
+
+        // 靜態屬性 static properties
+        Debug.Log(Random.value);
+        // 靜態方法 static method 
+        Debug.Log(Random.Range(1, 10));
+    }
+}
