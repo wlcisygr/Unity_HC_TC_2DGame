7353f1182027759b22498c0b2a2d7a62105ad90c
KID <50666207+KID421@users.noreply.github.com>
2019-11-13 21:05:57 +0800

    非靜態屬性

--- UnityProject/Assets/Scripts/LearnAPI.cs
+++ UnityProject/Assets/Scripts/LearnAPI.cs
@@ -2,6 +2,12 @@
 
 public class LearnAPI : MonoBehaviour
 {
+    public int speed = 0;
+
+    // 非靜態需要先定義欄位，並且在屬性面板上套用對應物件
+    public Transform bird;
+    public Transform ground;
+
     private void Start()
     {
         Debug.Log("一般!");
@@ -14,5 +20,9 @@ public class LearnAPI : MonoBehaviour
         Debug.Log(Random.value);
         // 靜態方法 static method 
         Debug.Log(Random.Range(1, 10));
+
+        // 非靜態屬性用法 properties
+        Debug.Log(bird.position);
+        Debug.Log(ground.position);
     }
 }
