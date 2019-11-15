66b3973f681c4f8a480b49d81080f69cd7084f52
KID <50666207+KID421@users.noreply.github.com>
2019-11-15 21:25:20 +0800

    認識設定、取得自訂類別的欄位

---
+++ UnityProject/Assets/Scripts/LearnAPITwo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LearnAPITwo : MonoBehaviour
+{
+    // 存放類別欄位
+    public Bird bird;
+
+    private void Start()
+    {
+        // 設定其他類別的欄位
+        bird.jump = 999;
+
+        // 取得其他類別的欄位
+        print("小雞的跳躍欄位：" + bird.jump);
+    }
+}
