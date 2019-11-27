a49b05428cd2e8622dadcfc85efa59c0f7fac933
KID <50666207+KID421@users.noreply.github.com>
2019-11-27 19:44:32 +0800

    練習使用剛體 API 並實作控制地心引力與推力

--- UnityProject/Assets/Scripts/Bird.cs
+++ UnityProject/Assets/Scripts/Bird.cs
@@ -8,6 +8,7 @@ public class Bird : MonoBehaviour
     public bool isDead;
 
     public GameObject goScore, goGM;
+    public Rigidbody2D r2d;     // 2D 剛體
 
     private void Update()
     {
@@ -23,6 +24,9 @@ public class Bird : MonoBehaviour
         {
             goScore.SetActive(true);
             goGM.SetActive(true);
+            
+            r2d.gravityScale = 1;                   // 2D 剛體.地心引力 = 1
+            r2d.AddForce(new Vector2(0, jump));     // 2D 剛體.推力(二為向量)
         }
     }
 
